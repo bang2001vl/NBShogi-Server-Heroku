142d23fa28718987d408787c4289f23aaad27ba1
Dragnell <bang2001vl@outlook.com.vn>
2021-07-20 15:08:44 +0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send("Hello world");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
