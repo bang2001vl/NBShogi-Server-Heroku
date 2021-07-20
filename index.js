214e63ead833db07ea53032d9e7348a408a2832b
Dragnell <bang2001vl@outlook.com.vn>
2021-07-20 15:55:32 +0700

    test auth

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) =>{
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+app.use(express.json());
+
+app.get('/home', (req, res) =>{
     res.send("Hello world");
 });
+
+app.get('/api/auth', (req, res) =>
+{
+  const err = req.error;
+  if(err) 
+  {
+    console.log("Error on /test: error = " + err);
+    return;
+  }
+  const code = req.query.code;
+  const username = req.query.username;
+  console.log("code = " + code + " ; username = "+ username);
+
+  const js = JSON.stringify(req.body);
+  console.log("JSON object received = " + js);
+})
 
 let port = process.env.PORT;
 if (port == null || port == "") {
